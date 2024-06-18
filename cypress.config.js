6c60fb49dd1ae4bd370c6578c97b79cc622ef66d
adelpoio <andredelpoiop@gmail.com>
2024-06-17 23:29:38 -0300

    Configure Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl:'https://notes-serverless-app.com',
     chromeWebSecurity: false,
+    projectId: "vy1hn1"
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    
   },
 });
