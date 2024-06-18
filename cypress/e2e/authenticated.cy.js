865912825088e63dc36c1c6bd069a2dbb278795c
adelpoio <andredelpoiop@gmail.com>
2024-06-18 00:22:14 -0300

    added custom commands.d.ts

--- cypress/e2e/authenticated.cy.js
+++ cypress/e2e/authenticated.cy.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 // cypress/e2e/authenticated.cy.js
+/// <reference path="../support/commands.d.ts" />
 
 import { faker } from '@faker-js/faker/locale/en'
 
@@ -36,10 +37,10 @@ describe('Scenarios where authentication is a pre-condition', () => {
       .should('be.equal', 'Complete')
   })
 
-
-  it('logs out', () => {
+  it('logs out', { tags: '@desktop-and-tablet' }, () => {
     cy.visit('/')
     cy.wait('@getNotes')
+
     if (Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')) {
       cy.get('.navbar-toggle.collapsed')
         .should('be.visible')
@@ -47,6 +48,7 @@ describe('Scenarios where authentication is a pre-condition', () => {
     }
 
     cy.contains('.nav a', 'Logout').click()
+
     cy.get('#email').should('be.visible')
   })
 })
